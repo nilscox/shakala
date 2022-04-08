97c3274b28906b3acbafc70a93bde4df35eaf45d
nilscox <nils@epita.fr>
2022-04-08 22:03:13 +0200

    add tailwind support

--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,34 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+
+import styles from './tailwind.css';
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossOrigin: 'anonymous',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,500;0,600;1,200;1,400;1,500;1,600&display=swap',
+    },
+    {
+      rel: 'stylesheet',
+      href: styles,
+    },
+  ];
+};
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'Shakala',
+  viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+const colors = require('tailwindcss/colors');
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+  content: ['./app/**/*.tsx'],
+  theme: {
+    fontFamily: {
+      body: ['Montserrat', 'sans-serif'],
+    },
+    fontSize: {
+      sm: '13px',
+      base: '16px',
+      lg: '18px',
+    },
+    spacing: {
+      0: '0px',
+      0.5: '4px',
+      1: '8px',
+      2: '12px',
+      3: '16px',
+      4: '24px',
+    },
+    colors: {
+      transparent: 'transparent',
+      white: colors.white,
+      primary: colors.amber[600],
+      dark: colors.slate[800],
+      'light-gray': colors.neutral[200],
+      text: colors.gray[900],
+      'text-light': colors.gray[500],
+      'text-white': colors.gray[100],
+    },
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              fontSize: '1.5rem',
+            },
+            h2: {
+              fontSize: '1.3rem',
+              marginTop: '1.5em',
+              marginBottom: '0.5em',
+            },
+            h3: {
+              fontSize: '1.1rem',
+              marginTop: '1.3em',
+              marginBottom: '0.3em',
+            },
+            p: {
+              marginTop: '0.7em',
+              marginBottom: '0.7em',
+              lineHeight: '1.4rem',
+            },
+            li: {
+              marginTop: 0.25,
+              marginBottom: 0.25,
+            },
+            strong: {
+              color: colors.gray[600],
+            },
+            a: {
+              color: colors.blue[600],
+              textDecoration: 'none',
+              fontWeight: 400,
+            },
+            blockquote: {
+              marginTop: '1em',
+              marginBottom: '1em',
+              color: 'inherit',
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
