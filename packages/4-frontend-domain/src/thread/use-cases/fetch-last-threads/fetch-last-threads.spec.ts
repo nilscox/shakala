05cd26c2bf1ca11cb062e6a161cf81658a08a11a
nils <nils@epita.fr>
2022-08-02 00:57:12 +0200

    prevent fetching the last threads when already fetched

---
+++ packages/4-frontend-domain/src/thread/use-cases/fetch-last-threads/fetch-last-threads.spec.ts
@@ -0,0 +1,29 @@
+import { createThread, TestStore } from '../../../test';
+import { selectLastThreads, setLastThreads } from '../../lists/last-threads';
+
+import { fetchLastThreads } from './fetch-last-threads';
+
+describe('fetchLastThreads', () => {
+  const store = new TestStore();
+
+  const thread = createThread();
+
+  beforeEach(() => {
+    store.threadGateway.getLast.mockResolvedValue([thread]);
+  });
+
+  it('fetches the last threads', async () => {
+    await store.dispatch(fetchLastThreads());
+
+    expect(store.threadGateway.getLast).toHaveBeenCalledWith(3);
+    expect(store.select(selectLastThreads)).toEqual([thread]);
+  });
+
+  it('does not fetch the last threads when already fetched', async () => {
+    store.dispatch(setLastThreads(['1']));
+
+    await store.dispatch(fetchLastThreads());
+
+    expect(store.threadGateway.getLast).not.toHaveBeenCalled();
+  });
+});
--- packages/4-frontend-domain/src/thread/use-cases/fetch-last-threads/fetch-last-threads.ts
+++ packages/4-frontend-domain/src/thread/use-cases/fetch-last-threads/fetch-last-threads.ts
@@ -2,11 +2,15 @@ import { getIds } from 'shared';
 
 import type { Thunk } from '../../../store';
 import { threadDtoToEntity } from '../../domain/thread-dto-to-entity';
-import { setLastThreads } from '../../lists/last-threads';
+import { selectLastThreads, setLastThreads } from '../../lists/last-threads';
 import { addThreads } from '../../thread.actions';
 
 export const fetchLastThreads = (): Thunk => {
   return async (dispatch, getState, { threadGateway }) => {
+    if (selectLastThreads(getState()).length > 0) {
+      return;
+    }
+
     const threads = await threadGateway.getLast(3);
 
     dispatch(addThreads(threads.map(threadDtoToEntity)));
