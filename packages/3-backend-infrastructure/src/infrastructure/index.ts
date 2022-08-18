de292a80ee25b09e350038b1053def974c4c6bcf
nils <nils@epita.fr>
2022-08-18 12:51:44 +0200

    test(backend): add unit tests for the bcrypt service

--- packages/3-backend-infrastructure/src/infrastructure/index.ts
+++ packages/3-backend-infrastructure/src/infrastructure/index.ts
@@ -6,7 +6,7 @@ export * from './http/http-errors';
 export * from './http/request';
 export * from './http/response';
 
-export * from './services/bcrypt.service';
+export * from './services/crypto/bcrypt.service';
 export * from './services/config.service';
 export * from './services/logger/console-logger.service';
 export * from './services/math-random-generator.service';
