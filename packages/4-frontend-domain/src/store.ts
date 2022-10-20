085a47c439ae1b69a7cb3e6e180bd111e1bdf11c
nilscox <nilscox.dev@gmail.com>
2022-10-20 21:17:03 +0200

    chore: add redux devtool

--- packages/4-frontend-domain/src/store.ts
+++ packages/4-frontend-domain/src/store.ts
@@ -1,3 +1,4 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
 import {
   AnyAction,
   applyMiddleware,
@@ -11,12 +12,14 @@ import { authenticationReducer } from './authentication/authentication.slice';
 import { commentsReducer } from './comment/comments.slice';
 import type { Dependencies } from './store.types';
 import { threadsReducer } from './thread/thread.slice';
+import { userActivitiesReducer } from './user/user-activities.slice';
 import { userReducer } from './user/user.slice';
 import { usersReducer } from './user/users.slice';
 
 const rootReducer = combineReducers({
   authentication: authenticationReducer,
   user: userReducer,
+  userActivities: userActivitiesReducer,
   users: usersReducer,
   threads: threadsReducer,
   comments: commentsReducer,
@@ -28,11 +31,10 @@ type AppThunkMiddleware = ThunkMiddleware<State, AnyAction, Dependencies>;
 type AppThunkDispatch = ThunkDispatch<State, Dependencies, AnyAction>;
 
 export const createStore = (dependencies: Dependencies, middlewares: Middleware[] = []) => {
-  return createReduxStore(
-    rootReducer,
-    applyMiddleware<AppThunkDispatch>(
-      thunkMiddleware.withExtraArgument(dependencies) as AppThunkMiddleware,
-      ...middlewares,
-    ),
+  const enhancer = applyMiddleware<AppThunkDispatch>(
+    thunkMiddleware.withExtraArgument(dependencies) as AppThunkMiddleware,
+    ...middlewares,
   );
+
+  return createReduxStore(rootReducer, composeWithDevTools(enhancer) as typeof enhancer);
 };
