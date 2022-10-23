63cc2c9b6109619ffc787f23b41aee5ab71c9208
nilscox <nilscox.dev@gmail.com>
2022-10-23 16:56:01 +0200

    chore: review build process

--- packages/3-backend-infrastructure/register-aliases.js
+++ packages/3-backend-infrastructure/register-aliases.js
@@ -3,7 +3,7 @@ const path = require('path');
 const moduleAlias = require('module-alias');
 
 moduleAlias.addAliases({
-  shared: path.resolve(__dirname, '..', '0-shared/dist/src'),
-  'backend-domain': path.resolve(__dirname, '..', '1-backend-domain/dist/src'),
-  'backend-application': path.resolve(__dirname, '..', '2-backend-application/dist/src'),
+  shared: path.resolve(__dirname, '..', '0-shared/dist'),
+  'backend-domain': path.resolve(__dirname, '..', '1-backend-domain/dist'),
+  'backend-application': path.resolve(__dirname, '..', '2-backend-application/dist'),
 });
