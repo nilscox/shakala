4f2c593f0dc6de33a5476f9a0722edfb1c4a3188
nilscox <nilscox.dev@gmail.com>
2022-10-24 21:16:36 +0200

    chore: change the image on the page not found view

--- packages/5-frontend-client/src/routes/not-found.tsx
+++ packages/5-frontend-client/src/routes/not-found.tsx
@@ -1,11 +1,15 @@
 import { PageTitle } from '~/components/layout/page-title';
-import notFoundImage from '~/images/404.png';
+import NotFoundImage from '~/images/page-not-found.svg';
+
+import { Link } from '../components/elements/link';
 
 export const NotFoundRoute = () => (
   <>
-    <PageTitle>Cette page n'existe pas</PageTitle>
+    <PageTitle>404 - Cette page n'existe pas</PageTitle>
     <h1 className="text-center">Cette page n'existe pas</h1>
-
-    <img src={notFoundImage} alt="not found" className="m-auto my-6 max-w-1 rounded-lg border bg-neutral" />
+    <Link to="/" className="block text-center">
+      Retour sur une page qui existe
+    </Link>
+    <NotFoundImage className="m-auto my-6 max-w-1 rounded-lg" />
   </>
 );
