dcd1793bb380bc509fcc2c6dddd7a8317000be5c
nilscox <nilscox.dev@gmail.com>
2022-12-07 22:41:40 +0100

    chore: change the highlighted comment styles

--- packages/5-frontend-client/tailwind.config.js
+++ packages/5-frontend-client/tailwind.config.js
@@ -126,7 +126,7 @@ module.exports = {
         '100%': { transform: 'scale(1)' },
       },
       highlight: {
-        '0%': { background: theme('colors.primary') + '33' },
+        '0%': { boxShadow: '0px 0px 10px 5px ' + theme('colors.primary') + '66' },
         '100%': {},
       },
       loading: {
