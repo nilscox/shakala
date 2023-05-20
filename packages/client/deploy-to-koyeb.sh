7c520406041ba62145f6068fb1af933f5571d507
nilscox <nilscox.dev@gmail.com>
2023-05-20 18:44:20 +0200

    chore(client): add environment variables to koyeb deploy script

--- packages/client/deploy-to-koyeb.sh
+++ packages/client/deploy-to-koyeb.sh
@@ -8,8 +8,19 @@ branch=
 
 port=
 
+no_index=
+
 api_internal_url=
 api_internal_port=
+
+analytics_site_url=
+analytics_site_id=
+
+contact_email=
+discord_url=
+roadmap_url=
+feedback_url=
+repository_url=
 
 koyeb service create "$service_name" \
   --app "$app_name" \
@@ -27,5 +38,13 @@ koyeb service create "$service_name" \
   --env NODE_ENV="production" \
   --env HOST="0.0.0.0" \
   --env PORT="$port" \
+  --env NO_INDEX="$no_index" \
   --env VITE_SERVER_API_URL="$api_internal_url:$api_internal_port" \
-  --env VITE_CLIENT_API_URL="/api"
+  --env VITE_CLIENT_API_URL="/api" \
+  --env VITE_ANALYTICS_URL="$analytics_site_url" \
+  --env VITE_ANALYTICS_SITE_ID="$analytics_site_id" \
+  --env VITE_CONTACT_EMAIL="$contact_email" \
+  --env VITE_DISCORD_URL="$discord_url" \
+  --env VITE_ROADMAP_URL="$roadmap_url" \
+  --env VITE_FEEDBACK_URL="$feedback_url" \
+  --env VITE_REPOSITORY_URL="$repository_url" \
